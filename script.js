ea6a1c49998a052e192a255f11ff58af25fe5bcf
Raniere Lima <raniereprojeto@gmail.com>
2022-02-26 19:21:02 -0300

    capturing form values

--- script.js
+++ script.js
@@ -79,12 +79,13 @@ const DOM = {
     
     addTransaction(transaction, index){
         const tr = document.createElement('tr')
-        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+        tr.dataset.index = index
 
         DOM.transactionsContainer.appendChild(tr)
     },
 
-    innerHTMLTransaction(transaction){
+    innerHTMLTransaction(transaction, index){
         const CSSclass = transaction.amount > 0 ? "income" : "expense"
 
         const amount = Utils.formatCurrency(transaction.amount)
@@ -94,7 +95,7 @@ const DOM = {
             <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
             <td>
-                <img src="./assets/minus.svg" alt="Remover Transação">
+                <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover Transação">
             </td>
         `
         return html
@@ -120,6 +121,19 @@ const DOM = {
 }
 
 const Utils = {
+    formatAmount(value){
+        value = Number(value) * 100
+
+        return value;
+    },
+
+    formatDate(date){
+        const splittedDate = date.split("-")
+
+        return (`${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`)
+    },
+
+
     formatCurrency(value){
         const signal = Number(value) < 0 ? "-" : ""
 
@@ -133,12 +147,71 @@ const Utils = {
         })
 
         return signal + value
-    }
+    },
+
 }
 
 const Form = {
+    description: document.querySelector('input#description'),
+    amount: document.querySelector('input#amount'),
+    date: document.querySelector('input#date'),
+
+    getValues(){
+        return{
+            description: Form.description.value,
+            amount: Form.amount.value,
+            date: Form.date.value
+        }
+    },
+
+    validateFields(){
+        const { description, amount, date } = Form.getValues()
+
+        if(
+            description.trim() === "" || 
+            amount.trim() === "" || 
+            date.trim() === "") 
+        {
+            throw new Error("Por favor, preencha todos os campos")
+        }
+    },
+
+    formatValues(){
+        let { description, amount, date } = Form.getValues()
+
+        amount = Utils.formatAmount(amount)
+
+        date = Utils.formatDate(date)
+
+        return {
+            description,
+            amount,
+            date
+        }
+
+    },
+
+    clearFields(){
+        Form.description.value = "",
+        Form.amount.value = "",
+        Form.date.value = ""
+    },
+
+
     submit(event){
         event.preventDefault()
+
+        try {
+            Form.validateFields()
+            const transaction = Form.formatValues()
+            Transaction.add(transaction)
+            Form.clearFields()
+            Modal.close()
+        } catch (error) {
+            alert(error.message)
+        }
+
+        
 
         
     }
@@ -147,9 +220,7 @@ const Form = {
 const App = {
     init(){
 
-        Transaction.all.forEach(transaction => {
-            DOM.addTransaction(transaction)
-        })
+        Transaction.all.forEach(DOM.addTransaction)
         
         DOM.updateBalance()
 
